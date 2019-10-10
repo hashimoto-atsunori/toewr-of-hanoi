22bc6c9449e3d12860941eee20c8a0f80f6921b9
hashimoto-atsunori <king.the.strength@gmail.com>
2019-10-10 23:43:47 +0900

    add counter

--- src/com/algorithm/hanoi/TowerOfHanoi.java
+++ src/com/algorithm/hanoi/TowerOfHanoi.java
@@ -4,11 +4,13 @@ public class TowerOfHanoi {
     public static void main (String[] args){
         hanoi('A', 'B', 'C', 3);
     }
-    static void hanoi (char x, char y, char z, int n){
+    static void hanoi (char a, char b, char c, int n){
+        int count = 0;
         if (n > 0){
-            hanoi(x, z, y, n-1);
-            System.out.println(x + " to " + z);
-            hanoi(y, x, z, n-1);
+            count++;
+            hanoi(a, c, b, n-1);
+            System.out.println(String.valueOf(count) + a + " to " + c);
+            hanoi(b, a, c, n-1);
         }
     }
 }
